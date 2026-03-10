55ddd9d401af9a8141e6edbc26868d1caf1fd776
Prathamesh Nithyanandan <71165940+PrathameshNithyanandan@users.noreply.github.com>
2026-03-10 10:14:35 +0530

    Develop (#7)
    * Add UseCase1HotelBookingApp with welcome message (#1)
    
    * Feature/uc2 (#2)
    
    * Add room initialization use case in BookMyStay
    
    * Add SingleRoom class extending Room
    
    * Create DoubleRoom class extending Room
    
    * Add SuiteRoom class extending Room
    
    * Feature/uc3 (#3)
    
    * Update BookMyStay.java
    
    * Implement RoomInventory class with room details
    
    * Feature/uc4 (#4)
    
    * Implement RoomSearchService for room availability search
    
    * Refactor UseCase3 to UseCase4 with room search
    
    * Feature/uc6 (#6)
    
    * Implement RoomSearchService for room availability search
    
    * Refactor UseCase3 to UseCase4 with room search
    
    * Rename class and implement booking request queue
    
    * Add Reservation class for managing guest bookings
    
    * Implement BookingRequestQueue and use case
    
    * Remove displayReservation method
    
    Removed displayReservation method from Reservation class.
    
    * Refactor BookingRequestQueue methods
    
    Removed displayBookingRequests method and main method. Added getNextRequest and hasRequests methods.
    
    * Refactor RoomInventory by removing unused fields
    
    Removed unused maps for beds, size, and price. Updated room type keys in roomAvailability.
    
    * Implement RoomAllocationService for booking management
    
    * Rename class and streamline booking requests
    
    Updated class name and refactored booking request handling.

--- BookingRequestQueue.java
+++ BookingRequestQueue.java
@@ -12,29 +12,12 @@ public void addBookingRequest(Reservation reservation) {
 bookingQueue.add(reservation);
 }
 
-public void displayBookingRequests() {
-for(Reservation r:bookingQueue) {
-r.displayReservation();
-System.out.println();
-}
-}
+public Reservation getNextRequest() {
+return bookingQueue.poll();
 }
 
-public class UseCase5BookingRequestQueue {
-public static void main(String[] args) {
-BookingRequestQueue requestQueue=new BookingRequestQueue();
-
-Reservation r1=new Reservation("Arun","Single");
-Reservation r2=new Reservation("Meena","Double");
-Reservation r3=new Reservation("Rahul","Suite");
-
-requestQueue.addBookingRequest(r1);
-requestQueue.addBookingRequest(r2);
-requestQueue.addBookingRequest(r3);
-
-System.out.println("Booking Request Queue");
-System.out.println();
-requestQueue.displayBookingRequests();
+public boolean hasRequests() {
+return !bookingQueue.isEmpty();
 }
 }
 
